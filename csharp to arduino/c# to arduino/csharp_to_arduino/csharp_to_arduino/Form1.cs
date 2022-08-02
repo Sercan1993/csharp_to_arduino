7664193fd041f84500834a087c5979f8e65f5e5d
Sercan1993 <oguzsercan940@gmail.com>
2022-08-02 15:19:41 +0300

    cs to arduino comands

---
+++ csharp to arduino/c# to arduino/csharp_to_arduino/csharp_to_arduino/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace csharp_to_arduino
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            serialPort1.PortName = "COM6";
+            serialPort1.BaudRate = 9600;
+            serialPort1.Open();
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            serialPort1.Write("1");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            serialPort1.Write("2");
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            serialPort1.Write("3");
+        }
+    }
+}
